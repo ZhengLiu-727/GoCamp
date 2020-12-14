42e709056313224d1ef969841a5e0941fa7be135
Zheng Liu <zhengliu.zhengliu@gmail.com>
2020-12-14 20:43:38 +0800

    add image upload and thumbnail virtual

--- seeds/index.js
+++ seeds/index.js
@@ -28,9 +28,18 @@ const seedDB = async () => {
             author: '5fd5d33d0d95ec0df0675f7e',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
-            price
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/zhengliu/image/upload/v1607944157/GoCamp/rrjgwr2s2rlxyre9t9fh.jpg',
+                    filename: 'GoCamp/rrjgwr2s2rlxyre9t9fh'
+                },
+                {
+                    url: 'https://res.cloudinary.com/zhengliu/image/upload/v1607940845/GoCamp/zkmkqivlpvqln8q2xkpy.jpg',
+                    filename: 'GoCamp/zkmkqivlpvqln8q2xkpy'
+                }
+            ]
         })
         await camp.save();
     }
